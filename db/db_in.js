f09467e0a03406344716a8f0686dcda934c52326
Ofis PC <ccsafarli@gmail.com>
2021-05-25 16:07:51 +0400

    medaxil updated

--- db/db_in.js
+++ db/db_in.js
@@ -4,15 +4,16 @@ const insertItem = async (req, res) => {
     console.log(req.body);
 
     const { ad, vahid, miqdar, tarix } = req.body;
-    const tarix_tz = tarix + "00:00 +04:00";
+    const tarix_tz = tarix + "T07:00+00:00";
 
     try {
-        await mypool.query(`INSERT INTO anbar(mehsul_adi, mehsul_vahidi, mehsul_miqdar, anbar_tarix)
-        VALUES ($1, $2, $3, $4);`, [ad, vahid, miqdar, tarix])
+        await mypool.query(`
+            INSERT INTO anbar(mehsul_adi, mehsul_vahidi, mehsul_miqdar, anbar_tarix)
+            VALUES ($1, $2, $3, $4);`, [ad, vahid, parseInt(miqdar), tarix_tz])
         res.status(200).send('Mehsul daxil edildi')
     }
     catch (e) {
-        throw new Error(`Error occured while inserting into anbar`)
+        throw new Error(`Error occured while inserting into anbar ${e}`)
     }
 }
 
