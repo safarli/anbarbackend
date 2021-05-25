5fdd7b602c2bf88a9ee3317594971ba69461eaf2
Ofis PC <ccsafarli@gmail.com>
2021-05-25 14:15:12 +0400

    handler functions changed to async await

--- db/db_out.js
+++ db/db_out.js
@@ -1,30 +1,39 @@
 const { mypool } = require('./db_conn.js');
 
-const getAll = (req, res) => {
-    mypool.query(`SELECT * FROM selectall_baku`)
-        .then((result) => {
-            const { rows } = result;
-            res.status(200).send(rows);
-        })
-        .catch(e => console.log(e))
+const getAll = async (req, res) => {
+    try {
+        const { rows } = await mypool.query(`SELECT * FROM selectall_baku`)
+        res.status(200).send(rows);
+    }
+    catch (e) {
+        throw new Error(`Error occured while selecting all records in Baku timezone ${e}`)
+    }
 }
 
-const getById = (req, res) => {
+const getById = async (req, res) => {
     const { id } = req.params;
-    mypool.query(`SELECT * FROM selectall_baku WHERE mehsul_id = $1`, [id])
-        .then((result) => {
-            const { rows } = result;
-            res.status(200).send(rows);
-        })
+
+    try {
+        const { rows } = await mypool.query(`SELECT * FROM selectall_baku WHERE id = $1`, [id])
+        res.status(200).send(rows)
+    }
+    catch (e) {
+        throw new Error(`Error occured while selecting: ${e}`)
+    }
 }
 
-const getByTimeInterval = (req, res) => {
-    const {startDate, endDate} = req.body;
+const getByTimeInterval = async (req, res) => {
+    const { startDate, endDate } = req.body;
     console.log(`START DATE: ${startDate}  END DATE: ${endDate}`)
 
-    mypool.query(`SELECT * FROM selectall_baku WHERE anbar_tarix BETWEEN $1 AND $2`, [startDate, endDate])
-        .then((result) => res.status(200).send(result.rows))
-        .catch(e => console.log(e));
+    try {
+        const { rows } = await mypool.query(`SELECT * FROM selectall_baku WHERE anbar_tarix BETWEEN $1 AND $2`, [startDate, endDate])
+        res.status(200).send(rows);
+    }
+    catch (e) {
+        throw new Error(`Error occured while selecting by time interval ${e}`)
+    }
+
 }
 
 module.exports = {
