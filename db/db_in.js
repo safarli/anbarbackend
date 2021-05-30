dffa8d24244931d40238b8c52789919df029dd95
safarli <wentonic@gmail.com>
2021-05-31 01:41:36 +0400

    a commit

--- db/db_in.js
+++ db/db_in.js
@@ -1,9 +1,22 @@
 const { mypool } = require('./db_conn');
 
 const insertItem = async (req, res) => {
+    if (!req.body) return res.status(500).json({ msg: "Request body not found!" })
     const { mehsultipi_id, satici_id, vahid, miqdar, tarix } = req.body;
     console.log(req.body);
-    res.status(200).json({msg: "OK"})
+
+    try {
+        const result = await mypool.query(`
+            INSERT INTO anbar(mehsultipi_id, satici_id, mehsul_vahidi, mehsul_miqdar, anbar_tarix)
+            VALUES
+            ($1, $2, $3, $4, $5) RETURNING *;`, [mehsultipi_id, satici_id, vahid, miqdar, tarix]);
+
+        res.status(200).json({msg: "Product added " + result})
+    }
+    catch (e) {
+        console.log(e.message)
+        res.status(406).send({message: e.message})
+    }
 }
 
 module.exports = {
