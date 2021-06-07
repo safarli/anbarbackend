677e65f0bc9a0e0c2db59efcbe80fae5ef9e486b
Ofis PC <ccsafarli@gmail.com>
2021-06-07 19:18:31 +0400

    a commit

--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -9,5 +9,5 @@ module.exports = function jwtGenerator(user_id, user_role) {
         }
     }
 
-    return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "30s" })
+    return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "30" })
 }
